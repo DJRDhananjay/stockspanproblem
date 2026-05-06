25cb53b899e266c41a5736bf5e952563d69289bd
DJRDhananjay <128344998+DJRDhananjay@users.noreply.github.com>
2026-05-06 21:04:17 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int prices[]={338,337,349,354,345,352};
+		int n=prices.length;
+		int span[]=new int[n];
+		Stack<Integer>stack=new Stack();
+		for(int i=0;i<n;i++){
+		    while(!stack.isEmpty()&&prices[stack.peek()]<=prices[i]){
+		        stack.pop();
+		    }
+		    if(stack.isEmpty()){
+		        span[i]=i+1;
+		    }
+		    else{
+		        span[i]=i-stack.peek();
+		    }
+		    stack.push(i);
+		}
+		System.out.print(Arrays.toString(span));
+	}
+}
